a7c77d0ead9316eff8c6d37abb4dbf5bab7a214f
samarasn <you@example.com>
2018-03-14 00:31:44 +0200

    first commit

---
+++ src/main/java/com/testclient/app/MyClientSocket.java
@@ -0,0 +1,37 @@
+package com.testclient.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyClientSocket {
+
+	private Socket socket;
+    private Scanner scanner;
+    private MyClientSocket(InetAddress serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+    private void start() throws IOException {
+    	System.out.println("start client: " );
+    	String input;
+    	long start = System.currentTimeMillis();
+    	long end = start + 60*1000; // 60 seconds * 1000 ms/sec
+        while (System.currentTimeMillis() < end) {
+        	this.socket.close();
+        	System.out.println("end connection");
+            input = scanner.nextLine();
+            System.out.println("This socket");            
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        MyClientSocket client = new MyClientSocket(
+                InetAddress.getByName("127.0.0.1"), Integer.parseInt("54010"));
+        
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start(); 
+    }
+}
