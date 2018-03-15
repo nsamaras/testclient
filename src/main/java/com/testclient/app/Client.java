546f33449de49a821aa9a52c4cff6c4c2bf10b47
TENANTS\samarasn <nikos.samaras@gmail.com>
2018-03-15 14:49:21 +0200

    new client and server

--- src/main/java/com/testclient/app/Client.java
+++ src/main/java/com/testclient/app/Client.java
@@ -2,67 +2,53 @@ package com.testclient.app;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
  
-public class Client
-{
- 
-    private static Socket socket;
- 
-    public static void main(String args[])
-    {
-        try
-        {
-            String host = "localhost";
-            int port = 5000;
-            InetAddress address = InetAddress.getByName(host);
-            socket = new Socket(address, port);
- 
-            //Send the message to the server
-            OutputStream os = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
- 
-            String firstMsgFromClient = "***********************\n";
+public class Client {
+	private static Socket socket;
 
-            bw.write(firstMsgFromClient);
-            bw.flush();
-            System.out.println("Message sent to the server : "+firstMsgFromClient);
- 
-            //Get the return message from the server
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String message = br.readLine();
-            System.out.println("Server Msg : " +message);
-            
-            // HI, I’M <name>
-            String name = MessagesUtil.generateName();
-            String msg = MessagesUtil.getClientHiMsg(name);
-            bw.write(msg);
-            bw.flush();
-            System.out.println("HI I'M " + name);
-            
-            
-            
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-        }
-        finally {
-            //Closing the socket
-            try {
-            	System.out.println("Wating 30 sec");            	
-//                socket.close();                
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public static void main(String args[])
+	{
+	    Scanner input=new Scanner(System.in);
+	    while(true)
+	    {
+	        try
+	        {
+	            String host = "localhost";
+	            int port = 25000;
+	            InetAddress address = InetAddress.getByName(host);
+	            socket = new Socket(address, port);
+	            //System.out.println("You're now connected to the Server"); /*this should only print once */
+	            //Send the message to the server
+	            OutputStream os = socket.getOutputStream();
+	            OutputStreamWriter osw = new OutputStreamWriter(os);
+	            BufferedWriter bw = new BufferedWriter(osw);
+
+	            String msg;
+	            msg=input.next();
+	            String sendMessage = msg + "\n";
+	            bw.write(sendMessage);
+	            bw.flush();
+	            System.out.println("Message sent to the server : "+sendMessage);
+
+	            //Get the return message from the server
+	            InputStream is = socket.getInputStream();
+	            InputStreamReader isr = new InputStreamReader(is);
+	            BufferedReader br = new BufferedReader(isr);
+	            String message = br.readLine();
+	            System.out.println("Message received from the server : " +message);
+	        }
+	        catch (IOException exception)
+	        {
+	            //System.out.println("Server is still offline");/*This should only print once*/
+	        }
+	    }
+	}
 }
--- src/main/java/com/testclient/app/Server.java
+++ src/main/java/com/testclient/app/Server.java
@@ -2,6 +2,9 @@ package com.testclient.app;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -12,55 +15,55 @@ import java.net.Socket;
 import java.util.UUID;
 
 public class Server {
-
 	private static Socket socket;
+	 public static void main(String[] args) throws IOException {
+	 try
+	    {
+	        int port = 25000;
+	        ServerSocket serverSocket = new ServerSocket(port);
+	        //Server is running always. This is done using this while(true) loop
+	        while(true)
+	        {
+	            //Reading the message from the client
+	            socket = serverSocket.accept();
+	            System.out.println("Client has connected!");
+	            InputStream is = socket.getInputStream();
+	            InputStreamReader isr = new InputStreamReader(is);
+	            BufferedReader br = new BufferedReader(isr);
+	            
+	            String msg = br.readLine();
+	            System.out.println("Message received from client is "+msg);
 
-	public static void main(String[] args) {
-		
-		String returnMessage;
-		
-		try {
-			int port = 5000;
-			ServerSocket serverSocket = new ServerSocket(port);
-			System.out.println("Server Started and listening to the port 5000 \n");
+	            //Multiplying the number by 2 and forming the return message
+	            String returnMessage;
+	            try
+	            {
+	            	String returnValue;
+	            	if("BYE".equals(msg)) {
+	            		returnValue = MessagesUtil.getServerEndMsg(msg, 30);
+	            	} else {
+	            		returnValue = "HELLO "+ msg;
+	            	}	                 
+	                returnMessage = String.valueOf(returnValue) + "\n";
+	            }
+	            catch(Exception e)
+	            {
+	                //Input was not a number. Sending proper message back to client.
+	                returnMessage = "Please send a proper number\n";
+	            }
 
-			// Server is running always. This is done using this while(true) loop
-			while (true) {
-				// Reading the message from the client
-				socket = serverSocket.accept();
-				InputStream is = socket.getInputStream();
-				InputStreamReader isr = new InputStreamReader(is);
-				BufferedReader br = new BufferedReader(isr);
-				UUID uuid = MessagesUtil.getUuid();
-				returnMessage = MessagesUtil.getServerHelloMsg(uuid);
-
-				// HI, I’M <session-id>
-				OutputStream os = socket.getOutputStream();
-				OutputStreamWriter osw = new OutputStreamWriter(os);
-				BufferedWriter bw = new BufferedWriter(osw);
-				bw.write(returnMessage);
-				bw.flush();
-				System.out.println("Message sent to the client is " + returnMessage);
-				
-				// HI <name>
-				InputStream is_2 = socket.getInputStream();
-				InputStreamReader isr_2 = new InputStreamReader(is_2);
-				BufferedReader br_2 = new BufferedReader(isr_2);
-				String name = br_2.readLine();
-				OutputStream os_2 = socket.getOutputStream();
-				OutputStreamWriter osw_2 = new OutputStreamWriter(os_2);
-				BufferedWriter bw_2 = new BufferedWriter(osw_2);
-				bw_2.write(MessagesUtil.getServerResponceMsg(name));
-				System.out.println(MessagesUtil.getServerResponceMsg(name));
-				bw_2.flush();
-			}
-		} catch (Exception e) {
-			returnMessage = MessagesUtil.getNotSupportedMsg();
-		} finally {
-			try {
-				socket.close();
-			} catch (Exception e) {
-			}
-		}
-	}
+	            //Sending the response back to the client.
+	            OutputStream os = socket.getOutputStream();
+	            OutputStreamWriter osw = new OutputStreamWriter(os);
+	            BufferedWriter bw = new BufferedWriter(osw);
+	            bw.write(returnMessage);
+	            System.out.println("Message sent to the client is "+returnMessage);
+	            bw.flush();
+	        }
+	    }
+	    catch (Exception e)
+	    {
+	        e.printStackTrace();
+	    }
+	 }
 }
